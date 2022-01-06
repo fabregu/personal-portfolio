c6660f88be4fd9fbafa2e6337ef9e27661a0abec
Freddy Abregu <fabregu01@gmail.com>
2022-01-06 08:21:05 -0500

    Subida de Proyecto

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Typíng Animation
+var typed = new Typed(".typing",{
+    strings:["", "Web Designer", "Web Developer", "Grafic Designer", "Youtuber"],
+    typeSpeed: 100,
+    BackSpeed:60,
+    loop: true
+}) 
